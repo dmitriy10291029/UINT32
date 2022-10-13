4b7bb9c50a0f2c9357a9ab9b1e9214fa3c95437b
dmitriy10291029 <dmitriy.lapshin10@gmail.com>
2022-10-13 03:02:38 +0300

    Start commit

---
+++ lib/number.cpp
@@ -0,0 +1,46 @@
+#include "number.h"
+#include <string>
+
+uint2022_t from_uint(uint32_t value) {
+    return uint2022_t();
+}
+
+uint2022_t from_string(const char* buff) {
+    return uint2022_t();
+}
+
+uint2022_t operator+(const uint2022_t& lhs, const uint2022_t& rhs) {
+    return uint2022_t();
+}
+
+uint2022_t operator-(const uint2022_t& lhs, const uint2022_t& rhs) {
+    return uint2022_t();
+}
+
+uint2022_t operator*(const uint2022_t& lhs, const uint32_t& x) {
+    return uint2022_t();
+}
+
+uint2022_t operator*(const uint32_t& x, const uint2022_t& lhs) {
+    return uint2022_t();
+}
+
+uint2022_t operator*(const uint2022_t& lhs, const uint2022_t& rhs) {
+    return uint2022_t();
+}
+
+uint2022_t operator/(const uint2022_t& lhs, const uint2022_t& rhs) {
+    return uint2022_t();
+}
+
+bool operator==(const uint2022_t& lhs, const uint2022_t& rhs) {
+    return false;
+}
+
+bool operator!=(const uint2022_t& lhs, const uint2022_t& rhs) {
+    return false;
+}
+
+std::ostream& operator<<(std::ostream& stream, const uint2022_t& value) {
+    return uint2022_t();
+}
---
+++ lib/number.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <cinttypes>
+#include <iostream>
+
+struct uint2022_t {
+
+};
+
+struct uint60_t {
+
+};
+
+uint2022_t from_uint(uint32_t i);
+
+uint2022_t from_string(const char* buff);
+
+uint2022_t operator+(const uint2022_t& lhs, const uint2022_t& rhs);
+
+uint2022_t operator-(const uint2022_t& lhs, const uint2022_t& rhs);
+
+uint2022_t operator*(const uint2022_t& lhs, const uint32_t& x);
+
+uint2022_t operator*(const uint32_t& x, const uint2022_t& lhs);
+
+uint2022_t operator*(const uint2022_t& lhs, const uint2022_t& rhs);
+
+uint2022_t operator/(const uint2022_t& lhs, const uint2022_t& rhs);
+
+bool operator==(const uint2022_t& lhs, const uint2022_t& rhs);
+
+bool operator!=(const uint2022_t& lhs, const uint2022_t& rhs);
+
+std::ostream& operator<<(std::ostream& stream, const uint2022_t& value);
